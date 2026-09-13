d58da870310e9e20d7ea3495e312c387202ccb96
yhsptraa <matthewyohsaputra@gmail.com>
2026-09-13 23:07:04 +0700

    feat: add button and input widgets

--- lib/core/widgets/app_button.dart
+++ lib/core/widgets/app_button.dart
@@ -1,1 +1,13 @@
+import 'package:flutter/material.dart';
 
+class AppButton extends StatelessWidget {
+  const AppButton({super.key, required this.label, required this.onPressed});
+
+  final String label;
+  final VoidCallback onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(onPressed: onPressed, child: Text(label));
+  }
+}
--- lib/core/widgets/app_input.dart
+++ lib/core/widgets/app_input.dart
@@ -1,1 +1,25 @@
+import 'package:flutter/material.dart';
 
+class AppInput extends StatelessWidget {
+  const AppInput({
+    super.key,
+    required this.label,
+    this.isPassword = false,
+    this.validator,
+  });
+
+  final String label;
+  final bool isPassword;
+  final String? Function(String?)? validator;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      obscureText: isPassword,
+      autocorrect: !isPassword,
+      enableSuggestions: !isPassword,
+      validator: validator,
+      decoration: InputDecoration(labelText: label),
+    );
+  }
+}
