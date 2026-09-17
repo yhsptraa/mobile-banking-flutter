5ad8f8a12fff70c791fbc9b4f2ce5b53f08a5cbd
yhsptraa <matthewyohsaputra@gmail.com>
2026-09-17 17:12:19 +0700

    feat: add Login Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'core/theme/app_theme.dart';
+import './features/login/screens/login_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,6 +15,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: AppTheme.lightTheme,
+      home: LoginScreen(),
     );
   }
 }
